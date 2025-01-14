f99e4aa592cf0bc2b9d530497bda66805beb53cc
Katerina <iglkotik@gmail.com>
2025-01-14 23:44:57 +0300

    Добавьте файлы проекта.

---
+++ les14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите имя кота");
+            string name_cat=Console.ReadLine();
+            Console.WriteLine("Напишите, что делает кот");
+            string povedenie_cat=Console.ReadLine();
+            Cat cat=new Cat(name_cat,povedenie_cat);
+            Console.WriteLine("Напишите имя собаки");
+            string name_dog = Console.ReadLine();
+            Console.WriteLine("Напишите, что делает собака");
+            string povedenie_dog = Console.ReadLine();
+            Dog dog = new Dog(name_dog, povedenie_dog);
+            cat.ShowInfo();
+            dog.ShowInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ les14/animals.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les14
+{
+    abstract class Animals
+    {
+        public abstract string Name { get; set; }
+
+        public Animals(string name) 
+        {
+            Name = name;
+        }
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+
+    }
+
+    class Cat : Animals
+    {
+        private string name;
+        private string povedenie;
+        public override string Name
+        { get => name;
+          set => name=value; 
+        }
+        public Cat(string name, string povedenie)
+            :base(name)
+        {
+            this.povedenie = povedenie;
+        }
+        public override void Say()
+        {
+            Console.WriteLine($"говорит Мяу и {povedenie}");
+        }
+    }
+
+    class Dog : Animals
+    {
+        private string name;
+        private string povedenie;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name, string povedenie)
+            : base(name)
+        {
+            this.povedenie = povedenie;
+        }
+        public override void Say()
+        {
+            Console.WriteLine($"говорит Гав-гав и {povedenie}");
+        }
+    }
+
+}
